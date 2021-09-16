01433bcc2d2148a596e60a9aaa78300a875af2d7
John Albright <John@john-albrights-macbook-pro.local>
2021-09-16 17:27:34 -0500

    Added authentification for Socket.IO and allowed user's name to be accessed from the socket

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+
+// Initialize a new memory store
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+// Have Socket.IO use the memory store
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
@@ -28,6 +48,8 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
+  store: store,
+  key: 'express.sid',
   cookie: { secure: false }
 }));
 
@@ -50,7 +72,8 @@ myDB(async (client) => {
   io.on('connection', socket => {
     ++currentUsers;
     // Emit user count
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
+    // console.log(socket.request.user);
 
     socket.on('disconnect', () => {
       --currentUsers;
@@ -66,6 +89,17 @@ myDB(async (client) => {
   });
 });;
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
